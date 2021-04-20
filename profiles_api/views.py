3b0fe5498ab8c3ced753a7b4a9509f0b7097efe6
thien.nguyen <thien.nguyen@altium.com>
2021-04-20 16:51:15 +0700

    crete api view and testing

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self,request,format=None):
+         an_apiview=[
+          'uses http method as function (get,post,path,put,delete)',
+          'is similar to a traditional  Django View',
+          'Gives you the most control over your application config',
+          'Is mapped manually to URLs'
+         ]
+
+         return Response({'message':'Hello','an_apiview':an_apiview})

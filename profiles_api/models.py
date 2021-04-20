178cc2fcdeafaad9e6af89c17525785152ccb130
thien.nguyen <thien.nguyen@altium.com>
2021-04-20 14:06:15 +0700

    create super admin user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -38,7 +38,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         return self.name
